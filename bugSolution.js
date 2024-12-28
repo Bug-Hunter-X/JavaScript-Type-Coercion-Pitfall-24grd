18633e79bc603f87a67f97d1c47b61ecbe276459
BugHunter-X <askzenai@gmail.com>
2024-12-28 08:56:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function foo(a,b){if(typeof a === 'number' && typeof b === 'number'){return a+b;}else{return "Error:Invalid input";} }console.log(foo(1,2)); // Output: 3console.log(foo(1,"2")); // Output: Error: Invalid input 
